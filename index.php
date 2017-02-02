76003c197d9e8c272870c2a23ea2a5bce10b1484
Paulo Henrique <paulocoliver@gmail.com>
2017-02-02 08:54:42 -0200

    - Adicionado os Registers (view, db) do Silex, necessários para o funcionamento da aplicação; - Criação da tabela stackoverflow caso não exista para armazenar as informações coletadas da API;

--- index.php
+++ index.php
@@ -6,6 +6,37 @@ use Symfony\Component\HttpFoundation\Request;
 $app = new Silex\Application();
 $app['debug'] = true;
 
+// REGISTER
+$app->register(new Silex\Provider\TwigServiceProvider(), [
+	'twig.path' => __DIR__.'/views',
+]);
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+	'db.options' => array(
+		'driver'   => 'pdo_sqlite',
+		'path'     => __DIR__.'/app.db',
+	),
+));
+/**
+ * @var $db \Doctrine\DBAL\Connection
+ */
+$db = $app['db'];
+
+
+if (!$db->getSchemaManager()->tablesExist('stackoverflow')) {
+	$res = $db->exec('CREATE TABLE stackoverflow(
+					   question_id INT PRIMARY KEY NOT NULL,
+					   title           TEXT,
+					   owner_name      TEXT,
+					   score           INT,
+					   creation_date   INT,
+					   link 		   TEXT,
+					   is_answered     INT);');
+	if (!$db->getSchemaManager()->tablesExist('stackoverflow'))
+		exit('Error create table stackoverflow');
+}
+
+
 // ROUTES
 $app->get('/', function () use ($app) {
 	return $app['twig']->render('index.twig', []);
