14cecd045d1ab9e924f3c3b6fa027cdc5296a2bb
Paulo Henrique <paulocoliver@gmail.com>
2017-02-02 09:16:01 -0200

    - Busca as ultimas 99 perguntas do stackoverflow via API que possui a TAG PHP; - Salva as perguntas na tabela stackoverflow; - Salva via arquivo o timestamp da última vez que os dados foram persistidos;

--- index.php
+++ index.php
@@ -42,4 +42,38 @@ $app->get('/', function () use ($app) {
 	return $app['twig']->render('index.twig', []);
 });
 
+$app->get('/get-api-stackoverflow', function() use($app, $db) {
+	try {
+		$client = new GuzzleHttp\Client();
+		$response = $client->request('GET', 'https://api.stackexchange.com/2.2/search/advanced?page=1&pagesize=99&order=desc&sort=activity&accepted=True&tagged=php&site=stackoverflow');
+		if ($response->getStatusCode() != 200)
+			throw new Exception('Status response error: '.$response->getStatusCode());
+
+		$res = json_decode($response->getBody());
+		if (empty($res->items))
+			throw new Exception('A API não retornou dados');
+
+		$db->exec('DELETE FROM stackoverflow;');
+		foreach ($res->items as $item) {
+			$db->insert('stackoverflow', [
+				'question_id' => $item->question_id,
+				'title' => $item->title,
+				'owner_name' => $item->owner->display_name,
+				'score' => $item->score,
+				'creation_date' =>  $item->creation_date,
+				'link' => $item->link,
+				'is_answered' => $item->is_answered,
+			]);
+		}
+
+		file_put_contents('cache_stackoverflow_time.txt', time());
+
+		$result = ['success' => 1];
+
+	} catch (Exception $e) {
+		$result = ['success' => 0, 'message' => 'Desculpe ocorreu um erro. '.$e->getMessage()];
+	}
+	return $app->json($result);
+});
+
 $app->run();
